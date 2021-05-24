497c7c75fb5d4f7da67371c56ae31ae5bad67ced
kezhenxu94 <kezhenxu94@apache.org>
2021-05-24 22:40:06 +0800

    Fix lint

--- cmd/start.go
+++ cmd/start.go
@@ -63,7 +63,7 @@ var startCmd = &cobra.Command{
 }
 
 func addShutDownHook(stopFunc func()) {
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
